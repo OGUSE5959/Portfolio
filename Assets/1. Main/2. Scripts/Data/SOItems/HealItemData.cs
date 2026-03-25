7dcecd829a2c3c38bbad21d093eab42be161ac06
OGUSE5959 <148595012+OGUSE5959@users.noreply.github.com>
2026-03-25 14:58:07 +0900

    1
    1

--- Assets/1. Main/2. Scripts/Data/SOItems/HealItemData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HealItemData : ConsumableItemData
-{
-    public override ConsumeType ConsumeType => ConsumeType.Heal;
-    public float healAmount;
-}
